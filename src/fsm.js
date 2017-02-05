6a5af9492248197b8358aa5c40f792755fa52f7a
Christopher-0118 <k.karantsevich@gmail.com>
2017-02-05 23:19:36 +0300

    finite-state-machine: implement some of methods

--- src/fsm.js
+++ src/fsm.js
@@ -3,19 +3,36 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
-
+    constructor(config) {
+        this.initial = config.initial;
+        this.states = config.states; 
+    }    
+        
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.initial;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        var flag = true;
+        
+        for (var key in this.states) {
+            if (key == state) {
+                this.initial = state;
+                flag = false;
+            }
+        }
+        if (flag) {
+            return new Error("State isn't exist");
+        } 
+    }
 
     /**
      * Changes state according to event transition rules.
@@ -55,7 +72,34 @@ class FSM {
      */
     clearHistory() {}
 }
-
+var config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+       
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
